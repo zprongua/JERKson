e4512ac6f2e5b97a40dd26f5da489b685111dc53
zprongua <115650795+zprongua@users.noreply.github.com>
2022-12-12 16:21:53 -0500

    Passing all the matching tests. Working on formatting output

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,6 +1,14 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GroceryReporter {
     private final String originalFileText;
@@ -11,6 +19,18 @@ public class GroceryReporter {
 
     @Override
     public String toString() {
-        return null;
+        ItemParser ip = new ItemParser();
+        List<Item> li = ip.parseItemList(originalFileText);
+        System.out.println(li.get(0));
+        Map<String, List<Double>> hm = li.stream().collect(Collectors.groupingBy(Item::getName,
+                Collectors.mapping(Item::getPrice, Collectors.toList())));
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (String k : hm.keySet()) {
+            sb.append(String.format("name:%8s \t\t seen: %s times\n============= \t\t =============\n", k, hm.get(k).size()));
+            sb.append(String.format("Price:%7s\t\t seen: %s times\n", hm.get(k).get(0), hm.get(k).size()));
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,55 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class ItemParser {
+
+    private int countOfErrors = 0;
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        List<Item> li = new LinkedList<>();
+        for (String s : valueToParse.split("##")) {
+            try {
+                li.add(parseSingleItem(s));
+            } catch (ItemParseException e) {
+                continue;
+            }
+        }
+        return li;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        singleItem = singleItem.toLowerCase();
+        Pattern element = Pattern.compile("\\W+\\w*[;@%*!^]|\\W+\\d+\\.\\d+[;@%*!^]|\\W+.+$");
+        Pattern good = Pattern.compile("name\\W\\w+\\Wprice\\W\\d+\\.\\d+\\Wtype\\W\\w+\\Wexpiration\\W.+$");
+        Matcher m = element.matcher(singleItem);
+        Matcher valid = good.matcher(singleItem);
+        Integer[] ia = new Integer[8];
+        int count = 0;
+        if (valid.find()) {
+            while (m.find()) {
+                ia[count] = m.start() + 1;
+                ia[count + 1] = m.end() - 1;
+                count += 2;
+            }
+            String name = singleItem.substring(ia[0], ia[1]);
+            Double price = Double.parseDouble(singleItem.substring(ia[2], ia[3]));
+            String type = singleItem.substring(ia[4], ia[5]);
+            String expiration = singleItem.substring(ia[6], ia[7] + 1);
+            if (expiration.substring(expiration.length()-2).equals("##")) expiration = expiration.substring(0, expiration.length()-2);
+            Item item = new Item(name, price, type, expiration);
+            return item;
+        }
+        countOfErrors++;
+        throw new ItemParseException();
+    }
+
+    public int getCountOfErrors() {
+        return countOfErrors;
     }
 }
